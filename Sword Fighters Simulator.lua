33f2f4eacde1926e649c6fad7cc48cd5a7e6c868
Awukiru <118483585+Awukiru@users.noreply.github.com>
2023-01-07 07:59:18 +0000

    Update Sword Fighters Simulator.lua

--- Sword Fighters Simulator.lua
+++ Sword Fighters Simulator.lua
@@ -352,7 +352,7 @@ if game.PlaceId == 11040063484 then
     end)
     
     task.spawn(function()
-        while task.wait() do
+        while task.wait(.1) do
             --// Auto Power
             if getgenv().AutoPower == true then
                 ClickRemotes.Click:InvokeServer();
